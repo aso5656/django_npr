b7e946696ec2bff4343ce848a3c08559b1ea9d8c
james_zheng <crisrooney@outlook.com>
2020-04-04 22:56:53 +0800

    cache url config

--- mysite/abola/urls.py
+++ mysite/abola/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import *
-
+from django.views.decorators.cache import cache_page
 app_name = 'abola'
 
 urlpatterns = [
-    path('',index, name='index'),
-    path('<str:team>',render_team,name='render_team'),
+    path('',cache_page(60*60*24*10)(index), name='index'),#cacge index page for 10 days
+    path('<str:team>', cache_page(60*15)(render_team),name='render_team'),
 ]
--- mysite/npr/urls.py
+++ mysite/npr/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-
+from django.views.decorators.cache import cache_page
 from . import views
 
 app_name = 'npr'
 
 urlpatterns = [
-    path('',views.IndexView.as_view(),name='index'),
+    path('',cache_page(60*60*24*7)(views.IndexView.as_view()),name='index'),#cache 7 days
     path('<str:order>/',views.IndexView.as_view(),name='index_by_order'),
     path('search',views.search,name='search'),
-    path('category/<slug:slug>',views.CategoryView.as_view(),name= 'by_category'),
-    path('<slug:slug>',views.PodcastView.as_view(),name='podcast'),
+    path('category/<slug:slug>',cache_page(60*60*24*7)(views.CategoryView.as_view()),name= 'by_category'), #cache 7 days
+    path('<slug:slug>',cache_page(60*30)(views.PodcastView.as_view()),name='podcast'), #cache 30 mins
     #path('podcast/<int:p_id>/<slug:slug>',views.update_episode,name='update_podcast'),
     path('podcast/<slug:slug>/<int:page>',views.PodcastView.as_view(),name='podcast_by_page'),
 ]
